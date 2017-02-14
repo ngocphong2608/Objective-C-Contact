4254163b553ef8efbcd33caacf162d80f0497bc0
ttphong <ttphong2608@gmail.com>
2017-02-14 11:35:50 +0700

    Initial Commit

---
+++ Contact/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contact
+//
+//  Created by CPU11808 on 2/14/17.
+//  Copyright © 2017 CPU11808. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Contact/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contact
+//
+//  Created by CPU11808 on 2/14/17.
+//  Copyright © 2017 CPU11808. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
