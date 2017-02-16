901c2c41e9678ea8cd860aa168e5c3426688ff85
ttphong <ttphong2608@gmail.com>
2017-02-16 15:39:48 +0700

    [add] ContactsViewController - Index List & Group

--- Contact/ContactsViewController.m
+++ Contact/ContactsViewController.m
@@ -10,6 +10,9 @@
 
 @interface ContactsViewController ()
 
+@property (strong, nonatomic) NSArray *contactIndexTitles;
+@property (strong, nonatomic) NSMutableDictionary *contactsDict;
+
 @end
 
 @implementation ContactsViewController
@@ -18,6 +21,12 @@
     [super viewDidLoad];
     
     [self.tableView setEditing:YES animated:YES];
+    
+    _contactIndexTitles = @[@"#", @"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z"];
+    
+    
+    
+    [self buildContactsDict];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,38 +34,67 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) buildContactsDict {
+    
+    // init contactsDict
+    _contactsDict = [NSMutableDictionary new];
+    for (NSString *contactIndex in _contactIndexTitles) {
+        [_contactsDict setObject:[NSMutableArray new] forKey:contactIndex];
+    }
+    
+    for (CSContact *contact in _contacts) {
+        [_contactsDict[[contact.fullName substringToIndex:1]] addObject:contact];
+    }
+    
+    NSLog(@"%@", _contactsDict);
+}
+
+
+
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return [_contactsDict count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    
+    return [_contactIndexTitles objectAtIndex:section];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    if (_contacts == NULL)
-        return 0;
-    else
-        return [_contacts count];
+    NSString *sectionTitle = [_contactIndexTitles objectAtIndex:section];
+    NSArray *sectionContacts = [_contactsDict objectForKey:sectionTitle];
+    return [sectionContacts count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     static NSString *CellIdentifier = @"ContactCell";
     
-    ContactCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    ContactCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     if (cell == nil) {
         cell = [[ContactCell alloc] initWithStyle:UITableViewCellStyleDefault
                                       reuseIdentifier:CellIdentifier];
     }
     
-    CSContact *contact = (self.contacts)[indexPath.row];
+    NSString *sectionTitle = [_contactIndexTitles objectAtIndex:indexPath.section];
+    NSArray *sectionContacts = [_contactsDict objectForKey:sectionTitle];
+    
+    CSContact *contact = [sectionContacts objectAtIndex:indexPath.row];
     
     [self setThumbnailImageForCell:cell withImageData:contact.thumbnailImageData orShortName:contact.get2CharatersByFullName];
   
     cell.nameLabel.text = contact.fullName;
     
     return cell;
+}
+
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
+{
+    return _contactIndexTitles;
 }
 
 - (void) setThumbnailImageForCell:(ContactCell *)cell withImageData:(NSData *)imageData orShortName:(NSString *) shortName{
